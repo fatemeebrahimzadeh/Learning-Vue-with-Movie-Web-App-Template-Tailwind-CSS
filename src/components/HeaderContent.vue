ee8bcd22b9d09989bc1322e0a6f555d802461f10
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-07-07 16:31:52 +0330

    add header social links

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,14 +1,17 @@
 <script setup>
 import HeaderNav from '@/components/HeaderNav.vue'
 import HeaderSearchBar from '@/components/HeaderSearchBar.vue'
+import SocialMediaLinksList from '@/components/SocialMediaLinksList.vue'
 </script>
 
 <template>
   <header class="w-full h-full bg-header bg-cover">
     <div class="w-full h-full bg-lighten dark:bg-transparent">
       <div class="container h-full flex flex-col justify-around">
+        <h1 class="hidden">MovieMaven</h1>
         <header-nav />
         <header-search-bar />
+        <social-media-links-list />
       </div>
     </div>
   </header>
