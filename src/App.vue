c511a50f485d2a392c53514ec0d3c4ef8682d060
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-07-07 23:06:05 +0330

    create home page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup>
-import HeaderContent from '@/components/HeaderContent.vue'
-import MainContent from '@/components/MainContent.vue'
-import FooterContent from '@/components/FooterContent.vue'
+import HomePage from '@/pages/HomePage.vue'
 </script>
 
 <template>
-  <header-content />
-  <main-content />
-  <footer-content />
+  <home-page />
 </template>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderContent from '@/components/HeaderContent.vue'
+import MainContent from '@/components/MainContent.vue'
+import FooterContent from '@/components/FooterContent.vue'
+</script>
+
+<template>
+  <header-content />
+  <main-content />
+  <footer-content />
+</template>
