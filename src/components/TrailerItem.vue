b469157c04fb313aae306defcbfa8d5a5ca28241
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-07-08 12:55:18 +0330

    make trailer section more dynamic with mock data

--- src/components/TrailerItem.vue
+++ src/components/TrailerItem.vue
@@ -1,13 +1,13 @@
 <script setup>
-import trailer from '@/assets/images/trailer2.jpg'
+defineProps(['src', 'name', 'duration'])
 </script>
 
 <template>
   <a href="#" class="movie-slide-item">
-    <img :src="trailer" alt="movie-slide-item2" />
+    <img :src="src" alt="movie-slide-item" />
     <div class="movie-slide-item-detail">
-      <h6 class="movie-slide-item-name text">Logan: Director James Mangold Interview</h6>
-      <span class="movie-slide-item-time">2:43</span>
+      <h6 class="movie-slide-item-name text">{{ name }}</h6>
+      <span class="movie-slide-item-time">{{ duration }}</span>
     </div>
   </a>
 </template>
--- src/components/TrailerSidebar.vue
+++ src/components/TrailerSidebar.vue
@@ -1,16 +1,62 @@
 <script setup>
 import TrailerItem from '@/components/TrailerItem.vue'
+import trailer2 from '@/assets/images/trailer2.jpg'
+import trailer3 from '@/assets/images/trailer3.png'
+import trailer4 from '@/assets/images/trailer4.png'
+import trailer5 from '@/assets/images/trailer5.jpg'
+import trailer6 from '@/assets/images/trailer6.jpg'
+import trailer7 from '@/assets/images/trailer7.jpg'
+
+const trailers = [
+  {
+    id: 1,
+    name: 'Logan: Director James Mangold Interview',
+    duration: '2:43',
+    src: trailer2
+  },
+  {
+    id: 2,
+    name: 'Logan: Director James Mangold Interview',
+    duration: '2:43',
+    src: trailer3
+  },
+  {
+    id: 3,
+    name: 'Logan: Director James Mangold Interview',
+    duration: '2:43',
+    src: trailer4
+  },
+  {
+    id: 4,
+    name: 'Logan: Director James Mangold Interview',
+    duration: '2:43',
+    src: trailer5
+  },
+  {
+    id: 5,
+    name: 'Logan: Director James Mangold Interview',
+    duration: '2:43',
+    src: trailer6
+  },
+  {
+    id: 6,
+    name: 'Logan: Director James Mangold Interview',
+    duration: '2:43',
+    src: trailer7
+  }
+]
 </script>
 
 <template>
   <div
     class="slider max-h-[435px] w-full lg:w-[400px] 2xl:w-[600px] overflow-y-scroll scroll-snap-y-mandatory"
   >
-    <trailer-item />
-    <trailer-item />
-    <trailer-item />
-    <trailer-item />
-    <trailer-item />
-    <trailer-item />
+    <trailer-item
+      v-for="trailer in trailers"
+      :key="trailer.id"
+      :name="trailer.name"
+      :duration="trailer.duration"
+      :src="trailer.src"
+    />
   </div>
 </template>
