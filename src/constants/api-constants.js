7b27b15cf4758222f19aaffe42a5bd360ea550b4
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-07-26 09:48:17 +0330

    add token in constant

--- src/constants/api-constants.js
+++ src/constants/api-constants.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = 'https://api.themoviedb.org'
 export const API_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/'
 export const API_VERSION = 3
-export const API_READ_ACCESS_TOKEN = import.meta.env.VITE_API_READ_ACCESS_TOKEN
+export const API_READ_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmQ5MTBhNDQxNzI5NmQ5MDczZjJlNmUzZjNlMDk1NyIsInN1YiI6IjY0YTJjZDhiMTEzODZjMDBhZGM3OWNjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UCEqmnJ1SMmALiGWVThtFffX-_Kvj1qHkUlmjehNehk"
